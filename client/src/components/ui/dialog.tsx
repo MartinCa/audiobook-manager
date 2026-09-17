a907a83442aeaf3d6034546477b82b7b714ec63d
Martin Caspersen <martin@caspersen.me>
2026-09-17 21:23:35 +0200

    chore(client): import cn directly in vendored ui components

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Dialog as DialogPrimitive } from "@base-ui/react/dialog";
 
-import { cn } from "@/lib/utils";
+import { cn } from "cn";
 import { Button } from "@/components/ui/button";
 import { XIcon } from "lucide-react";
 
